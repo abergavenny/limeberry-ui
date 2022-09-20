f5b7e1d1a15f447bc86a288a022e142f2531bbad
abergavenny <abergavenny@protonmail.ch>
2022-09-20 19:43:14 +0200

    Version 0.0.6-a

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
 import { App } from "vue"
-
+import * as components from "./components"
 import './assets/main.css'
 
 export interface LimeberryOptions {
     components?: Record<string, any>;
 }
 
-const createLimeberry = (options: LimeberryOptions = {}) => {
+const createLimeberryPlugin = (options: LimeberryOptions = {}) => {
     const install = (app: App) => {
-        const { components } = options
-
-        for (const key in components) {
-            app.component(key, components[key])
+        const plugins: Record<string, any> = components;
+        for (const key in plugins) {
+            app.component(key, plugins[key])
         }
     }
 
     return { install }
 }
 
-export * as components from "./components"
-
-export default createLimeberry
+export default createLimeberryPlugin
